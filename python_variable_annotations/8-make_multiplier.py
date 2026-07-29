8f452dd3fe289fce388fa09c1d2463af8fe8d74f
maryem <zanned.maryem@gmail.com>
2026-07-29 19:47:13 +0100

    add 8-make_multiplier.py file

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """This module defines a type‑annotated function make_multiplier."""
-
-
 from typing import Callable
+
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
