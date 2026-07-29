f315fd07f46bbd0b2e169166e307a69d8b5c286b
maryem <zanned.maryem@gmail.com>
2026-07-29 19:04:21 +0100

    add 6-sum_mixed_list.py file

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This module conatins a annotated function tha sums the elements of a list"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+    """A function that returns the sum of a list elements"""
+    sum: float = 0.0
+    for element in mxd_lst:
+        sum += element
+    return sum
