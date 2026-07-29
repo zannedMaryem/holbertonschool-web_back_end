7f7434d621fdbcec3c7a65b7b2b439b84b15d888
maryem <zanned.maryem@gmail.com>
2026-07-29 18:38:42 +0100

    add 2-floor.py file

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module conatins an annotated concatinationfloor function"""
+import math
+
+
+def floor(n: float) -> int:
+    """ A function that returns the floor of a float"""
+    return math.floor(n)
