45ecb9174178a891fbc8df75d9a5dfdc2bdf16be
maryem <zanned.maryem@gmail.com>
2026-07-29 18:59:04 +0100

    add 5-sum_list.py file

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """This module conatins a annotated function tha sums the elements of a list"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ A function that returns the sum of a list elements"""
     num: float = 0.0
     for element in input_list:
