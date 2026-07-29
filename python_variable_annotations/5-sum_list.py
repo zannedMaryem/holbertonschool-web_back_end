7e78d25f7b962d7fb0d9b9f3e17c267bf3e8d2c3
maryem <zanned.maryem@gmail.com>
2026-07-29 18:57:06 +0100

    add 5-sum_list.py file

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -4,6 +4,7 @@
 
 def sum_list(input_list: list[float]) -> float:
     """ A function that returns the sum of a list elements"""
+    num: float = 0.0
     for element in input_list:
         num += element
     return num
