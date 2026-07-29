19e56c2f9a699d2544e33b270ed4b41391f465e9
maryem <zanned.maryem@gmail.com>
2026-07-29 19:41:21 +0100

    add 7-to_kv.py file

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """A function that returns a tuple"""
     return (k, float(v ** 2))
