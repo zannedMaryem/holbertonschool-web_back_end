e46698fa33a480af70dcea2c6ab620f6145ea4cd
maryem <zanned.maryem@gmail.com>
2026-08-22 16:06:54 +0100

    add 3-hypermedia_del_pagination.py file

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -40,6 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """ Retunrs Deletion-resilient hypermedia pagination"""
         assert type(page_size) is int and page_size > 0
 
         if index is None:
