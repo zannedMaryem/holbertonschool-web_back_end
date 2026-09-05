1ba7c68a666203f3dba2ec5a34f62c197e381f91
maryem <zanned.maryem@gmail.com>
2026-09-05 13:54:53 +0100

    add 5-typed_arrays.js file

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,17 @@
-export default function createInt8TypedArray(length, position, value){
-    const Arr = new Int8Array(length);
-    if (Arr.length < position){
-        throw new Error('Position outside range');
-    }
-    Arr[position] = value;
-    return Arr;
+// 5-typed_arrays.js
+export default function createInt8TypedArray(length, position, value) {
+  // Create a buffer of the given length
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+
+  // Check position validity
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  // Set the value at the given position
+  view.setInt8(position, value);
+
+  // Return the DataView
+  return view;
 }
