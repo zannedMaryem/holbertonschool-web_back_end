a7f26e30c0dd44d86030fc77e8820f3e5aa5bbe7
maryem <zanned.maryem@gmail.com>
2026-09-05 16:37:46 +0100

    add 10-update_uniq_items.js file

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+export default function updateUniqueItems(map) {
+    if (!(map instanceof Map)) {
+        throw new Error('Cannot process');
+    }
+
+    map.forEach((quantity, item) => {
+        if (quantity === 1) {
+            map.set(item, 100);
+        }
+    });
+
+    return map;
+}
