5536921bf176ef3555df91b26f8dd5c90024e48e
maryem <zanned.maryem@gmail.com>
2026-09-05 16:19:16 +0100

    add 8-clean_set.js file

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 export default function cleanSet(set, startString) {
+    if (!startString) {
+        return '';
+    }
+
     return [...set]
         .filter((value) => value.startsWith(startString))
         .map((value) => value.slice(startString.length))
