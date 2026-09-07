d71dfaa1da7165680c2cce24e9f3f89c860c3b66
maryem <zanned.maryem@gmail.com>
2026-09-07 22:33:47 +0100

    add 6-final-user.js file

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,9 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    return Promise.allSettled([
+        signUpUser(firstName, lastName),
+        uploadPhoto(fileName),
+    ]);
+}
