f4aa67dbbd81cc706e409d8f82af5030c3c48c68
maryem <zanned.maryem@gmail.com>
2026-09-07 21:46:11 +0100

    add 2-then.js file

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+export default function handleResponseFromAPI(promise) {
+    return Promise.resolve(promise)
+        .then(() => ({
+            status: 200,
+            body: 'success',
+        }))
+        .catch(() => new Error())
+        .finally(() => {
+            console.log('Got a response from the API');
+        });
+}
