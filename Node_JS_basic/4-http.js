754afff00f3897b0001749c93d7efdfc566aac48
maryem <zanned.maryem@gmail.com>
2026-09-11 15:06:19 +0100

    add 4-http.js file

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
