ada5e5c8bc23d8e0602f302163024b463cacdbc3
Maryem ZANNED <35015196+zannedMaryem@users.noreply.github.com>
2026-09-24 14:18:58 +0100

    update 6-http_express.js

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    response.send('Hello Holberton School!');
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
 });
 
 app.listen(1245);
